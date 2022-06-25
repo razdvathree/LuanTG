9c266965a106fe5758f57cd3dc651a93cfc3dbcc
Azuremods <97257024+Azuremods@users.noreply.github.com>
2022-06-25 23:59:02 +0300

    Create delmod.py

---
+++ plugins/delmod.py
@@ -0,0 +1,18 @@
+from pyrogram import Client,filters
+from plugins.help import module_list
+from configurator import prefix
+import os
+
+
+@Client.on_message(filters.command("delmod", prefixes=prefix) & filters.me)
+async def delete_module(client,message):
+  try:
+    mod_for_delete = message.text.split(f'{prefix}delmod ')[1]
+  except:
+    await message.edit('This module is not available')
+  try:
+    os.remove(f'plugins/{mod_for_delete}')
+    await message.edit('The module has been successfully removed!')
+  except Exception as e:
+    await message.edit('Oops error: ',e)
+    
